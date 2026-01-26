a54d4cbf63ad30463c2a4af3815b25dbf9f2a6d3
sauhardh <sauhardhakafle12@gmail.com>
2026-01-26 18:39:37 +0000

    Fix: typo on rust_analyzer

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -29,7 +29,7 @@ return {
         end,
         default_settings = {
           -- rust-analyzer language server configuration
-          ["rust-analyzer"] = {
+          ["rust_analyzer"] = {
             cargo = {
               allFeatures = true,
               loadOutDirsFromCheck = true,
